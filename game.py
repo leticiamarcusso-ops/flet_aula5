9eb481d14a23d79d1fc9c35bd371e9f751e7409e
leticiamagre <leticia.marcusso@aluno.senai.br>
2026-02-13 11:53:54 -0300

    atividade 5

--- game.py
+++ game.py
@@ -1,23 +1,53 @@
 import flet as ft
 
 def main(page: ft.Page):
+    mensagem= ft.Text("Escolha o nome certo!")
+    resposta_correta = "Henry Danger"
+
+    def verificar_resposta(e):
+        if e.control.content == resposta_correta:
+            mensagem.value = "Parabens"
+        else:
+            mensagem.value = "Resposta errada"
+        page.update()
+
     page.title = "Game: Adivinhe a Imagem"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.bgcolor = "blue"
 
     page.add(
        ft.Column(
            controls=[
                ft.Text(
-                   "Adivinhe a Imagem",
+                   "Qual o nome do ajudante do capitao MEN",
                    size=24,
                    weight="bold"
                ),
                ft.Image(
-                   src="image/gatinho.jpg",
+                   src="image/henry.jpg",
                    height=200
-               )
-           ]   
+               ),
+               ft.Row(
+                   controls=[
+                       ft.Button(
+                           content="Henry Danger",
+                           on_click=verificar_resposta
+                       ),
+                       ft.Button(
+                           content="Jasper",
+                           on_click=verificar_resposta
+                       ),
+                       ft.Button(
+                           content="Schwoz",
+                           on_click=verificar_resposta
+                       ),
+                   ],
+                   alignment=ft.MainAxisAlignment.CENTER
+               ),
+               mensagem
+           ],
+           horizontal_alignment=ft.CrossAxisAlignment.CENTER   
        )
     )
 
